91d30b2acd862a404471d6886a65fde10b1f41c7
Oleg Levantin <od@datalab.fit>
2019-10-05 16:31:28 +0300

    Sort imports, del trash from db_methods

--- db_methods.py
+++ db_methods.py
@@ -1,8 +1,8 @@
 import sqlalchemy as sa
 from sqlalchemy import create_engine
-from utils import datetime_now
-from orm_tables import wishlist
 
+from orm_tables import wishlist
+from utils import datetime_now
 
 
 class Database():
@@ -11,7 +11,7 @@ class Database():
         engine = create_engine(engine_settings, pool_recycle=3600)
         self._conn = engine.connect()
     
-    def select_from_wishlist(self, status: list = ["Active", "Done", "Delete"]):
+    def select_from_wishlist(self, status=["Active", "Done", "Delete"]):
         notes = list()
         query = sa.select([wishlist]).where(wishlist.c.status.in_(status)).order_by(sa.desc(wishlist.c.tms_update))
         for row in self._conn.execute(query):
@@ -52,15 +52,3 @@ class Database():
                                              wishlist.c.note_id == note_id
                                          )
         self._conn.execute(query)
-
-
-
-if __name__ == "__main__":    
-    a = Database(db_type="mysql+pymysql",
-                name="root",
-                password="asd",
-                host="localhost",
-                port="3306",
-                database="wishlist")
-
-    print(a.get_note_data(1))
